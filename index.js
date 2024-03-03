0ff804d5c85e865ea74ad621795a4be053260ce2
Aaron Echler <44124054+acechler@users.noreply.github.com>
2024-03-03 11:45:13 -0500

    Document what I learned.

--- index.js
+++ index.js
@@ -1,5 +1,13 @@
 // Codewars: https://www.codewars.com/kata/514b92a657cdc65150000006/train/javascript
 
+
+/// What I learned
+// Using a set in javascript allows for you to create a list WITHOUT duplicate elements
+// This can be helpful to prevent someone from traversing an array for the same elements.
+// 
+// By using an array and checking it each time for a duplicate will make the time complexity
+// 0(n^2)
+
 function solution(number) {
 
   /// Guard against negative arguements.
